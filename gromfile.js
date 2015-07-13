68cea7a488f029d659bce7025be7b48170a56e6e
Dmitriy Kharchenko <akimoto@yandex.ru>
2015-07-13 12:35:03 +0700

    cli is about to work now

--- gromfile.js
+++ gromfile.js
@@ -3,9 +3,16 @@
 module.exports.test = function* (){
   var source = yield this.source("adasdadddd")
   var js = yield coffee(source, {})
+  yield this.seq([
+    another({}),
+    coffee({})
+
+  ])
 }
 
 
-var another = function* (options){
-  return "asdasd"
+var another = function (options){
+  return function* (files){
+
+  }
 }
